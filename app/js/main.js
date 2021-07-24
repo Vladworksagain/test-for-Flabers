a2a9c75b4c65d95a6e10ca18a42d4708ecd131ab
stassmoke <stanislav.kulunych@gmail.com>
2021-07-24 19:30:22 +0300

    'init'

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+function openCity(evt, id) {
+    let i,
+        tabcontent,
+        tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+
+    document.getElementById(id).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// document.addEventListener("DOMContentLoaded", () => {
+//
+// })
+//
+//
+// function getTabButtons() {
+//     document.
+// }
