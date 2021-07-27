6819f07d1d015eafe30e48b525e3505601d13e55
vladworksagain <vladworksagain@gmail.com>
2021-07-27 16:58:56 +0300

    reviews

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+let index
+
 function tabs(evt, id) {
     let i,
         tabcontent,
@@ -6,6 +8,7 @@ function tabs(evt, id) {
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
+        tabcontent[i].classList.remove('active')
     }
 
     tablinks = document.getElementsByClassName("tablinks");
@@ -14,8 +17,35 @@ function tabs(evt, id) {
     }
 
     document.getElementById(id).style.display = "block";
+    document.getElementById(id).classList.add('active')
     evt.currentTarget.className += " active";
+
+
+
+    // window.addEventListener('resize', () => {
+    //     const width = window.innerWidth;
+    //     const tab = document.querySelectorAll('.tablinks')
+    //     tab.forEach((tabs) => {
+    //         if(width <= 868 && !tabs.classList.contains('active')) {
+    //             tabs.style.display = 'none';
+    //         }
+    //     })
+    // })
+
+    index = id
 }
+
+const buttonNext = document.querySelectorAll('.feedback-content__btn-next');
+
+buttonNext.forEach((buttonNextStep) => {
+    buttonNextStep.addEventListener('click', nextStep);
+})
+
+function nextStep(e) {
+    e.preventDefault()
+    const element = document.getElementById(Number(index) + 1)
+}
+
 
 document.querySelector('.tablinks').click()
 
@@ -26,9 +56,13 @@ var phoneMask = IMask(element, {
     placeholderChar: '_',
 });
 
+
+
+
 let map;
 
 function initMap() {
+
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: -34.397, lng: 150.644 },
         zoom: 8,
